dc9538835abdf80e4959b8ab88e7d51bf1ba6f22
k1151204 <lastking77@naver.com>
2015-12-30 16:18:28 +0900

    Test
    Test

---
+++ Assets/Z_TestingAssets/ProCamera2D/Examples/Platformer/Scripts/ToggleCinematics.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Com.LuisPedroFonseca.ProCamera2D;
+
+namespace Com.LuisPedroFonseca.ProCamera2D.Platformer
+{
+    public class ToggleCinematics : MonoBehaviour
+    {
+        public ProCamera2DCinematics Cinematics;
+
+        void OnGUI()
+        {
+            if (GUI.Button(new Rect(5, 5, 180, 30), (Cinematics.IsActive ? "Stop" : "Start") + " Cinematics"))
+            {
+                if (Cinematics.IsActive)
+                    Cinematics.Stop();
+                else
+                    Cinematics.Play();
+            }
+        }
+    }
+}
